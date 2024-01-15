5044d52dcde49e70e1596af882f33ec3869cc2ec
Ilias G <igoujgali@gmail.com>
2024-01-15 15:15:42 +0100

    spring base

---
+++ src/test/java/ch/morgias/cookgenda/CookgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.morgias.cookgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
