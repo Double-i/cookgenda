e505f669a7997b79b4345669337bbcad2bb139b0
Ilias G <igoujgali@gmail.com>
2024-01-29 21:03:20 +0100

    create interfaces for food and recipe services

---
+++ src/main/java/ch/morgias/cookgenda/services/food/RecipeService.java
@@ -0,0 +1,19 @@
+package ch.morgias.cookgenda.services.food;
+
+import ch.morgias.cookgenda.models.food.Recipe;
+import ch.morgias.cookgenda.models.food.dto.AddFoodToRecipeDto;
+import ch.morgias.cookgenda.models.food.dto.RecipeEditDto;
+
+import java.util.Collection;
+
+public interface RecipeService {
+    Collection<Recipe> getAllRecipes();
+
+    void createRecipe(RecipeEditDto recipeDto);
+
+    void addFoodToRecipe(Long recipeId, AddFoodToRecipeDto addFoodToRecipeDto);
+
+    Recipe findRecipeById(Long recipeId);
+
+    void deleteRecipeById(Long recipeId);
+}
