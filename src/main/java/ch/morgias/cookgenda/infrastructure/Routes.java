a50065fbee9c1db53407c157e8b58d73f16cd842
Ilias G <igoujgali@gmail.com>
2024-02-03 16:03:02 +0100

    add routes for planing recipe

--- src/main/java/ch/morgias/cookgenda/infrastructure/Routes.java
+++ src/main/java/ch/morgias/cookgenda/infrastructure/Routes.java
@@ -4,5 +4,6 @@ public class Routes {
     public static final String BASE_API ="/api/v1";
     public static final String FOOD_INDEX = BASE_API + "/foods";
     public static final String RECIPE_INDEX = BASE_API + "/recipes";
+    public static final String PLANED_RECIPE_CREATE = BASE_API + "/recipes/{recipeId}/planed";
 
 }
