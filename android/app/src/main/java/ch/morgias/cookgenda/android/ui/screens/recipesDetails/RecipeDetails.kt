8ec39f9e3f940fe713e80d0b30a054fd2544164f
Ilias G <igoujgali@gmail.com>
2024-03-25 15:27:43 +0100

    update viewmodel

--- android/app/src/main/java/ch/morgias/cookgenda/android/ui/screens/recipesDetails/RecipeDetails.kt
+++ android/app/src/main/java/ch/morgias/cookgenda/android/ui/screens/recipesDetails/RecipeDetails.kt
@@ -1,12 +1,12 @@
 package ch.morgias.cookgenda.android.ui.screens.recipesDetails
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,14 +22,13 @@ import ch.morgias.cookgenda.android.ui.screens.common.Loading
 fun RecipeDetails(recipeId: Int, recipeDetailViewModel: RecipeDetailsViewModel) {
     LaunchedEffect(recipeDetailViewModel) {
         recipeDetailViewModel.getRecipeDetail(recipeId) // Déclenche le fetch des données
-        Log.i("RecipeDetails", "données recette ")
     }
-    when (recipeDetailViewModel.recipeUiState) {
+    when (val state = recipeDetailViewModel.uiState.collectAsState().value) {
         RequestState.Error -> ErrorLoading()
         RequestState.Loading -> Loading()
         is RequestState.Success<*> -> {
             val details =
-                (recipeDetailViewModel.recipeUiState as RequestState.Success<RecipeDetails>).result
+                (state as RequestState.Success<RecipeDetails>).result
 
             Row() {
                 Image(painterResource(id = R.drawable.recipe), contentDescription = "Oui")
