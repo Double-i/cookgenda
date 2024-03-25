47a8e969aad292f80f8033903dd8695c3919bf02
Ilias G <igoujgali@gmail.com>
2024-03-25 15:27:27 +0100

    update viewmodel

--- android/app/src/main/java/ch/morgias/cookgenda/android/ui/screens/recipesDetails/RecipeDetailsViewModel.kt
+++ android/app/src/main/java/ch/morgias/cookgenda/android/ui/screens/recipesDetails/RecipeDetailsViewModel.kt
@@ -1,29 +1,30 @@
 package ch.morgias.cookgenda.android.ui.screens.recipesDetails
 
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ch.morgias.cookgenda.android.network.RecipeApi
 import ch.morgias.cookgenda.android.network.RequestState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class RecipeDetailsViewModel : ViewModel() {
-    var recipeUiState: RequestState by mutableStateOf(RequestState.Loading)
-        private set
+    private val _recipeUiState: MutableStateFlow<RequestState> =
+        MutableStateFlow(RequestState.Loading)
+    val uiState: StateFlow<RequestState> = _recipeUiState.asStateFlow()
 
 
     fun getRecipeDetail(id: Int) {
         viewModelScope.launch {
-            recipeUiState = try {
+            _recipeUiState.value = try {
+                Log.i("RecipeDetailsViewModel - getRecipeDetail", "données recette ")
                 RequestState.Success(RecipeApi.retrofitService.getRecipe(id))
             } catch (ex: Exception) {
                 Log.e("t", ex.message!!, ex.cause)
                 RequestState.Error
             }
-            Log.i("VM", recipeUiState.toString())
         }
     }
 }
