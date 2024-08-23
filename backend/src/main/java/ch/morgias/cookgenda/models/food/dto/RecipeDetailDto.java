63e2a8d5603ce438e800b0d9cfb7ccec4b2e3496
Ilias G <igoujgali@gmail.com>
2024-08-23 21:38:49 +0200

    adapt and create dto for recipe details screen

--- backend/src/main/java/ch/morgias/cookgenda/models/food/dto/RecipeDetailDto.java
+++ backend/src/main/java/ch/morgias/cookgenda/models/food/dto/RecipeDetailDto.java
@@ -3,6 +3,8 @@ package ch.morgias.cookgenda.models.food.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class RecipeDetailDto {
@@ -10,4 +12,5 @@ public class RecipeDetailDto {
     private String name;
     private String description;
     private String image;
+    private List<RecipeFoodDto> recipeFoods;
 }
---
+++ backend/src/main/java/ch/morgias/cookgenda/models/food/dto/RecipeFoodDto.java
@@ -0,0 +1,11 @@
+package ch.morgias.cookgenda.models.food.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RecipeFoodDto {
+    private String foodName;
+    private Double quantity;
+}
