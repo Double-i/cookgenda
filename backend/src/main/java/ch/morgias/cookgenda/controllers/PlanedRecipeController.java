80d14ed86f8bf5195e34e957469656abc623e2df
ilias <igoujgali@gmail.com>
2024-08-24 17:45:30 +0200

    Create formattage

--- backend/src/main/java/ch/morgias/cookgenda/controllers/PlanedRecipeController.java
+++ backend/src/main/java/ch/morgias/cookgenda/controllers/PlanedRecipeController.java
@@ -1,6 +1,8 @@
 package ch.morgias.cookgenda.controllers;
 
 import ch.morgias.cookgenda.infrastructure.Routes;
+import ch.morgias.cookgenda.models.agenda.PlanedRecipe;
+import ch.morgias.cookgenda.models.agenda.PlaningDayDto;
 import ch.morgias.cookgenda.models.agenda.dto.EditPlanedRecipeDto;
 import ch.morgias.cookgenda.models.agenda.dto.PlanedRecipeDto;
 import ch.morgias.cookgenda.models.agenda.dto.mappers.PlanedRecipeMapper;
@@ -9,21 +11,42 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Collection;
+import java.util.*;
 
 @RestController
 @RequiredArgsConstructor
 public class PlanedRecipeController {
 
     private final PlanedRecipeService planedRecipeService;
+
     @PostMapping(Routes.PLANED_RECIPE_CREATE)
     public PlanedRecipeDto plannedRecipe(@PathVariable Long recipeId, @RequestBody EditPlanedRecipeDto planedRecipeDto) {
         return PlanedRecipeMapper.INSTANCE.toPlanedRecipeDto(planedRecipeService.planRecipe(recipeId, planedRecipeDto));
     }
+
     @GetMapping(Routes.PLANED_RECIPE_INDEX_PERIOD)
-    public Collection<PlanedRecipeDto> getPlannedRecipe(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime from,
-                                                        @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime to) {
-        return PlanedRecipeMapper.INSTANCE.toPlanedRecipeDtoList(planedRecipeService.findPlanedRecipeByPeriod(from, to));
+    public Collection<PlaningDayDto> getPlannedRecipe(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime from,
+                                                      @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime to) {
+        Collection<PlanedRecipe> t = planedRecipeService.findPlanedRecipeByPeriod(from, to);
+        // TODO déplacer dans un endroit plus approprié (mapper ? service)
+        LocalDate d = LocalDate.from(from);
+        Map<Long, List<PlanedRecipe>> map = new HashMap<>();
+        for (PlanedRecipe t2 : t) {
+            while (d.isAfter(to.toLocalDate())) {
+                // Create a list if not exists (we want empty list for empty days)
+                if (!map.containsKey(d.toEpochDay())) {
+                    map.put(d.toEpochDay(), new ArrayList<>());
+                }
+                if (!t2.getPlanedDate().toLocalDate().equals(d)) {
+                    continue;
+                }
+                map.get(d.toEpochDay()).add(t2);
+            }
+            d = d.plusDays(1);
+        }
+
+        return PlanedRecipeMapper.INSTANCE.toPlanedRecipeDtoV2List(map);
     }
 }
