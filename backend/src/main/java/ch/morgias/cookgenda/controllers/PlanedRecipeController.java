edc7e3a358af4d5f99ae4661686d82055bf6a123
Ilias G <igoujgali@gmail.com>
2024-08-24 22:10:28 +0200

    use localdate instead of localdatetime since searching by hour is not very useful for now

--- backend/src/main/java/ch/morgias/cookgenda/controllers/PlanedRecipeController.java
+++ backend/src/main/java/ch/morgias/cookgenda/controllers/PlanedRecipeController.java
@@ -12,7 +12,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.*;
 
 @RestController
@@ -27,14 +27,14 @@ public class PlanedRecipeController {
     }
 
     @GetMapping(Routes.PLANED_RECIPE_INDEX_PERIOD)
-    public Collection<PlaningDayDto> getPlannedRecipe(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime from,
-                                                      @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime to) {
-        Collection<PlanedRecipe> t = planedRecipeService.findPlanedRecipeByPeriod(from, to);
+    public Collection<PlaningDayDto> getPlannedRecipe(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate from,
+                                                      @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate to) {
+        Collection<PlanedRecipe> t = planedRecipeService.findPlanedRecipeByPeriod(from.atStartOfDay(), to.atTime(LocalTime.MAX));
         // TODO déplacer dans un endroit plus approprié (mapper ? service)
         Map<Long, List<PlanedRecipe>> map = new HashMap<>();
         for (PlanedRecipe t2 : t) {
             LocalDate d = LocalDate.from(from);
-            while (d.isBefore(to.toLocalDate())) {
+            while (d.isBefore(to)) {
                 // Create a list if not exists (we want empty list for empty days)
                 if (!map.containsKey(d.toEpochDay())) {
                     map.put(d.toEpochDay(), new ArrayList<>());
