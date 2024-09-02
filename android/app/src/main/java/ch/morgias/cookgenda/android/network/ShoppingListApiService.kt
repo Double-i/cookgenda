b47f8e25c5f23929911a150721c160c616d08fbc
Ilias G. <igoujgali@gmail.com>
2024-09-02 14:26:21 +0200

    create API call method for generation shopping list

--- android/app/src/main/java/ch/morgias/cookgenda/android/network/ShoppingListApiService.kt
+++ android/app/src/main/java/ch/morgias/cookgenda/android/network/ShoppingListApiService.kt
@@ -3,7 +3,9 @@ package ch.morgias.cookgenda.android.network
 import ch.morgias.cookgenda.android.models.ShoppingList
 import ch.morgias.cookgenda.android.models.ShoppingListResume
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
+import java.time.LocalDate
 
 interface ShoppingListApiService {
 
@@ -12,6 +14,12 @@ interface ShoppingListApiService {
 
     @GET("shoppingLists/{shoppingListId}")
     suspend fun getShoppingList(@Path("shoppingListId") id: Int): ShoppingList
+
+    @POST("shoppingLists")
+    suspend fun generateShoppingList(
+        @Path("shoppingListId") startDate: LocalDate,
+        endDate: LocalDate
+    ): ShoppingListResume
 
 }
 
--- android/app/src/main/java/ch/morgias/cookgenda/android/ui/screens/shoppingList/ShoppingListsViewModel.kt
+++ android/app/src/main/java/ch/morgias/cookgenda/android/ui/screens/shoppingList/ShoppingListsViewModel.kt
@@ -8,6 +8,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+
+typealias GenerateShoppingSuccessCallback = (id: Long) -> Unit
 
 class ShoppingListsViewModel : ViewModel() {
     private val _shoppingListsUiState: MutableStateFlow<RequestState> =
@@ -21,6 +24,25 @@ class ShoppingListsViewModel : ViewModel() {
         viewModelScope.launch {
             _shoppingListsUiState.value = try {
                 RequestState.Success(ShoppingListApi.retrofitService.getShoppingListResumes())
+            } catch (err: Exception) {
+                RequestState.Error
+            }
+        }
+    }
+    fun generateNewShoppingList(
+        startDate: LocalDate,
+        endDate: LocalDate,
+        callback: GenerateShoppingSuccessCallback
+    ) {
+        viewModelScope.launch {
+            try {
+                val t = ShoppingListApi.retrofitService.generateShoppingList(
+                    startDate,
+                    endDate
+
+                )
+                callback(t.id)
+
             } catch (err: Exception) {
                 RequestState.Error
             }
