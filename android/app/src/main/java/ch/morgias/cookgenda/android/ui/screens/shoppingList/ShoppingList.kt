364e18b84429dcb336ce9b3c471fa3dc1f8ee98c
Ilias G. <igoujgali@gmail.com>
2024-09-03 16:18:47 +0200

    wip shopping list

--- android/app/src/main/java/ch/morgias/cookgenda/android/ui/screens/shoppingList/ShoppingList.kt
+++ android/app/src/main/java/ch/morgias/cookgenda/android/ui/screens/shoppingList/ShoppingList.kt
@@ -1,17 +1,24 @@
 package ch.morgias.cookgenda.android.ui.screens.shoppingList
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Checkbox
-
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import ch.morgias.cookgenda.android.models.ShoppingList
 import ch.morgias.cookgenda.android.network.RequestState
@@ -37,12 +44,32 @@ fun ShoppingList(viewModel: ShoppingListViewModel, shoppingListId: Int) {
                 LazyColumn {
                     listInfo.shoppingListFoods.keys.toList().forEach {
                         item {
-                            Text("Titre")
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(50.dp)
+                                    .background(
+                                        Color.Blue
+                                    ),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+
+                                Spacer(modifier = Modifier.height(15.dp))
+                                Text("Titre", style = MaterialTheme.typography.titleLarge)
+                                Spacer(modifier = Modifier.height(10.dp))
+                            }
                         }
                         items(listInfo.shoppingListFoods[it]!!) { foodItem ->
-                            Row {
-                                Column {
-                                    Text(foodItem.name)
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    viewModel.checkShoppingListFoodWithId(
+                                        foodItem.id,
+                                        !foodItem.checked
+                                    )
+                                }) {
+                                Column(modifier = Modifier.weight(1F)) {
+                                    Text(foodItem.name, style = MaterialTheme.typography.bodyLarge)
                                     Text(foodItem.quantity.toString())
                                 }
                                 Checkbox(checked = foodItem.checked, onCheckedChange = {
